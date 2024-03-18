aef052573690ec901f27c0281f0bc59fb746330c
oliverfelix <jayandhans484@gmail.com>
2024-03-19 03:40:34 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from loggingg import logging
 from src.Parkinson.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.Parkinson.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,5 +13,16 @@ if __name__ == '__main__':
         obj.main()
         logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
+        logging.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Data Validation stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logging.exception(e)
         raise e
